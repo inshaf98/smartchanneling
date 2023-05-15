286e9e7650b2ff7359cb8938b0b11b5aa920aa4c
Inshaf <mohammedinshaf495@gmail.com>
2023-05-15 23:45:25 +0530

    static fix1

--- smartchanneling/settings.py
+++ smartchanneling/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'smartapp/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'smartapp/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
